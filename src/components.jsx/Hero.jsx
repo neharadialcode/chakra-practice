56c3a9c35500e77b8d735a42653b91bdc03dce69
Neha <neharadialcode@gmail.com>
2024-02-12 15:15:00 +0530

    break point

--- src/components.jsx/Hero.jsx
+++ src/components.jsx/Hero.jsx
@@ -4,25 +4,36 @@ import React from "react";
 const Hero = () => {
   return (
     <>
-      <Box maxW="1000px">
-        <Text fontSize="xl">
-          Paystack helps businesses in Africa get paid by anyone, anywhere in
-          the world
+      <Box bg="light_green" borderTop="1px solid #cbdcd9">
+        <Text fontSize={{ base: "24px", md: "40px", lg: "56px" }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          nesciunt neque, impedit accusamus consequuntur a dignissimos
+          veritatis, ipsum molestias ut facilis at perferendis? Eius, nulla sunt
+          mollitia porro id laboriosam.
         </Text>
-        <Heading>
-          The first Ai-Generated token.
-          <Text color="green" as="span">
-            Ever.
-          </Text>
-        </Heading>
-        <Button
-          size="lg"
-          bg="green"
-          mt="24px"
-          _hover={{ transform: "scale(1.1)" }}
+        <Box
+          maxW="1140px"
+          mx="auto"
+          py="20px"
+          px="10px"
+          h="100vh"
+          textAlign="center"
         >
-          Create a free account
-        </Button>
+          <Heading fontSize="3xl" pt="10%">
+            The first Ai-Generated token.
+            <Text color="green" as="span">
+              Ever.
+            </Text>
+          </Heading>
+          <Button
+            size="lg"
+            bg="green"
+            mt="24px"
+            _hover={{ transform: "scale(1.1)" }}
+          >
+            Create a free account
+          </Button>
+        </Box>
       </Box>
     </>
   );
