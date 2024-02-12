2dc3f2bf2812f9abc11ba6f01215c8940a5c3bca
Neha <neharadialcode@gmail.com>
2024-02-12 12:21:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "./App.css";
+import Hero from "./components.jsx/Hero";
 
 function App() {
+  const theme = extendTheme({
+    colors: {
+      green: {
+        50: "red",
+        100: "blue",
+        500: "#4caf50",
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Hero />
+      <Button size="lg" bg="green.50" mt="24px">
+        Create a free account
+      </Button>
+    </ChakraProvider>
   );
 }
 
